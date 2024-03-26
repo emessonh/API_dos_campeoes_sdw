919acf5b9bf11ac16e528a278ffd1831c8cc0809
EMESSON HOR�CIO DOS SANTOS <2022005052@MACEIO.IFAL>
2024-03-26 17:05:56 -0300

    inicio projeto

---
+++ API-dos-campeoes/src/main/java/me/dio/API/dos/campeoes/ApiDosCampeoesApplication.java
@@ -0,0 +1,13 @@
+package me.dio.API.dos.campeoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDosCampeoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDosCampeoesApplication.class, args);
+	}
+
+}
---
+++ API-dos-campeoes/src/test/java/me/dio/API/dos/campeoes/ApiDosCampeoesApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.API.dos.campeoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDosCampeoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
